d5f971d94aa5a8af35e2d0ff0fd7e888f90a5dc8
ght <597315261@qq.com>
2017-10-25 12:12:04 +0800

    添加必要的注释

--- library/src/main/java/com/gzsll/jsbridge/WVJBConstants.java
+++ library/src/main/java/com/gzsll/jsbridge/WVJBConstants.java
@@ -5,9 +5,9 @@ package com.gzsll.jsbridge;
  */
 public class WVJBConstants {
     public static final String TAG = "WebViewJavascriptBridge";
-    public static final String INTERFACE = TAG + "Interface";
-    public static final String SCHEME = "wvjbscheme";
-    public static final String MESSAGE = "__WVJB_QUEUE_MESSAGE__";
-    public static final String BRIDGE_LOADED="__BRIDGE_LOADED__";
+    public static final String INTERFACE = TAG + "Interface"; //WebViewJavascriptBridgeInterface
+    public static final String SCHEME = "wvjbscheme"; //  wvjbscheme://
+    public static final String MESSAGE = "__WVJB_QUEUE_MESSAGE__";//  wvjbscheme://__WVJB_QUEUE_MESSAGE__
+    public static final String BRIDGE_LOADED="__BRIDGE_LOADED__";//  wvjbscheme://__BRIDGE_LOADED__
 
 }
